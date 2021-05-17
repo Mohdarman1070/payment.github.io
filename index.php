63d08b8738659a1d5acb2acf3e2ddcb991bdab43
arman <armanmohammad786786@gmail.com>
2021-05-17 18:00:51 +0530

    fisrt commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<form action="pay.php" method="post">
+    <header class="head">
+    <div class="container">
+
+    Name: <input type="text" name="customer" placeholder="type your name">
+    <br>
+    Email: <input type="text" name="Email" placeholder="type your Email">
+    <br>
+    contactno: <input type="text" name="contactno" placeholder="type your contactno" >
+    <br>
+        price: <input type="text" name="price">
+        <br>
+
+        </div>
+    <div class="btn">
+    <button>DONATE NOW</button>
+    </div>
+   
+    </header>
+    </form>
+</body>
+</html>
